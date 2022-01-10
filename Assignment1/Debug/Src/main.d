f534d49c3bce7a9c39279df11c96d40b2d1b5bc1
huyns1998 <syhuynguyen1@gmail.com>
2022-01-10 15:28:50 +0700

    assignment2

---
+++ Assignment1/Debug/Src/main.d
@@ -0,0 +1,37 @@
+Src/main.o: ../Src/main.c \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/misc.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_exti.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_syscfg.h
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/misc.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_exti.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_syscfg.h:
---
+++ Assignment3/Debug/Src/main.d
@@ -0,0 +1,76 @@
+Src/main.o: ../Src/main.c \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/eventman.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/led/led.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/melody.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/buzzer.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/lightsensor.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/temhumsensor.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/eventbutton.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/button.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/serial/uartcmd.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/serial/serial.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_usart.h \
+ E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/misc.h
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/eventman.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/led/led.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/melody.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/buzzer.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/lightsensor.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/temhumsensor.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/eventbutton.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/button.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/serial/uartcmd.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/serial/serial.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_usart.h:
+
+E:/Drive\ C/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/misc.h:
